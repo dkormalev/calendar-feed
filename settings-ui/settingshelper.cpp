b7e9f0581d7623b1f4af7894dd798504926f8033
Denis Kormalev <kormalev.denis@gmail.com>
2011-12-09 01:37:17 +0400

    Forgotten license headers

--- settings-ui/settingshelper.cpp
+++ settings-ui/settingshelper.cpp
@@ -1,3 +1,27 @@
+/**************************************************************************
+**
+** This file is part of Calendar Feed
+**
+** Copyright (C) 2011  Denis Kormalev <kormalev.denis@gmail.com>
+**
+**
+** GNU Lesser General Public License Usage
+**
+** This file may be used under the terms of the GNU Lesser General Public
+** License version 2.1 as published by the Free Software Foundation and
+** appearing in the file LICENSE.LGPL included in the packaging of this file.
+** Please review the following information to ensure the GNU Lesser General
+** Public License version 2.1 requirements will be met:
+** http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+**
+** This application is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU Lesser General Public License for more details.
+**
+**
+**************************************************************************/
+
 #include "settingshelper.h"
 #include <QDBusConnection>
 #include <QFile>
