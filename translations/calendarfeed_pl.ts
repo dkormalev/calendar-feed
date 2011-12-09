786c5a4f4878fc37800d319fd5d230b618847b57
Denis Kormalev <kormalev.denis@gmail.com>
2011-12-10 01:20:24 +0400

    Polish translation added

---
+++ translations/calendarfeed_pl.ts
@@ -0,0 +1,126 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="pl">
+<context>
+    <name></name>
+    <message id="calendar_feed_subtitle">
+        <location filename="../settings-ui/qml/AboutPage.qml" line="80"/>
+        <source>Calendar events at Feed screen</source>
+        <translation>Zdarzenia kalendarzowe na ekranie kanału</translation>
+    </message>
+    <message id="calendar_feed_about_gitorious_page">
+        <location filename="../settings-ui/qml/AboutPage.qml" line="97"/>
+        <source>Project page at Gitorious</source>
+        <translation>Strona projektu na Gitorious</translation>
+    </message>
+    <message id="calendar_feed_about_contact_email">
+        <location filename="../settings-ui/qml/AboutPage.qml" line="106"/>
+        <source>Contact e-mail</source>
+        <translation>Kontakt e-mailowy</translation>
+    </message>
+    <message id="calendar_feed_about_help_page">
+        <location filename="../settings-ui/qml/AboutPage.qml" line="115"/>
+        <source>Help page</source>
+        <translation>Strona pomocy</translation>
+    </message>
+    <message id="calendar_feed_about_thanks_to">
+        <location filename="../settings-ui/qml/AboutPage.qml" line="124"/>
+        <source>Special thanks to</source>
+        <translation>Specjalne podziękowania</translation>
+    </message>
+    <message id="calendar_feed_refresh">
+        <location filename="../settings-ui/qml/MainPage.qml" line="61"/>
+        <source>Refresh Feed Item</source>
+        <translation>Odśwież element</translation>
+    </message>
+    <message id="calendar_feed_about">
+        <location filename="../settings-ui/qml/MainPage.qml" line="66"/>
+        <source>About</source>
+        <translation>O programie</translation>
+    </message>
+    <message id="calendar_feed_title">
+        <location filename="../settings-ui/qml/MainPage.qml" line="98"/>
+        <source>Calendar Feed</source>
+        <translation>Kanał kalendarza</translation>
+    </message>
+    <message id="calendar_feed_setting_group_behavior">
+        <location filename="../settings-ui/qml/MainPage.qml" line="104"/>
+        <source>Behavior</source>
+        <translation>Zachowanie</translation>
+    </message>
+    <message id="calendar_feed_setting_publish_to_feed">
+        <location filename="../settings-ui/qml/MainPage.qml" line="111"/>
+        <source>Publish to Feed</source>
+        <translation>Opublikuj na kanela</translation>
+    </message>
+    <message id="calendar_feed_setting_fill_with_future">
+        <location filename="../settings-ui/qml/MainPage.qml" line="119"/>
+        <source>Fill with Future Events</source>
+        <translation>Wypełnij przyszłymi zdarzeniami</translation>
+    </message>
+    <message id="calendar_feed_setting_limit_future">
+        <location filename="../settings-ui/qml/MainPage.qml" line="128"/>
+        <source>Limit Future Events (days)</source>
+        <translation>Ogranicz ilość przyszłych zdarzeń (dni)</translation>
+    </message>
+    <message id="calendar_feed_setting_group_display">
+        <location filename="../settings-ui/qml/MainPage.qml" line="152"/>
+        <source>Appearance</source>
+        <translation>Wygląd</translation>
+    </message>
+    <message id="calendar_feed_setting_events_number">
+        <location filename="../settings-ui/qml/MainPage.qml" line="158"/>
+        <source>Events Shown</source>
+        <translation>Prezentowane zdarzenia</translation>
+    </message>
+    <message id="calendar_feed_setting_show_calendar_bar">
+        <location filename="../settings-ui/qml/MainPage.qml" line="169"/>
+        <source>Show Calendar Color</source>
+        <translation>Pokarz kolor kalendarza</translation>
+    </message>
+    <message id="calendar_feed_setting_highlight_today">
+        <location filename="../settings-ui/qml/MainPage.qml" line="178"/>
+        <source>Highlight Today Events</source>
+        <translation>Podświetl dzisiejsze zdarzenia</translation>
+    </message>
+    <message id="calendar_feed_setting_date_format">
+        <location filename="../settings-ui/qml/MainPage.qml" line="187"/>
+        <location filename="../settings-ui/qml/MainPage.qml" line="188"/>
+        <source>Date Format</source>
+        <translation>Format daty</translation>
+    </message>
+    <message id="calendar_feed_refresh_error">
+        <location filename="../settings-ui/settingshelper.cpp" line="92"/>
+        <location filename="../settings-ui/settingshelper.cpp" line="117"/>
+        <location filename="../settings-ui/settingshelper.cpp" line="125"/>
+        <location filename="../settings-ui/settingshelper.cpp" line="133"/>
+        <source>Internal OS error. Please, reboot your device.</source>
+        <translation>Błąd systemowy. Proszę zrestartować urządzenie.</translation>
+    </message>
+    <message id="calendar_feed_item_title">
+        <location filename="../settings-ui/settingshelper.cpp" line="138"/>
+        <source>Calendar</source>
+        <translation>Kalendarz</translation>
+    </message>
+    <message id="calendar_feed_item_no_events">
+        <location filename="../settings-ui/settingshelper.cpp" line="140"/>
+        <source>No events in your calendar.</source>
+        <translation>Brak zdarzeń w kalendarzu.</translation>
+    </message>
+    <message id="calendar_feed_setting_calendars_shown">
+        <location filename="../settings-ui/settingshelper.cpp" line="142"/>
+        <source>Calendars Shown</source>
+        <translation>Prezentowane kalendarze</translation>
+    </message>
+    <message id="calendar_feed_setting_refresh_interval">
+        <location filename="../settings-ui/qml/MainPage.qml" line="141"/>
+        <source>Refresh Interval (minutes)</source>
+        <translation>Częstotliwość odświeżania (minuty)</translation>
+    </message>
+    <message id="calendar_feed_setting_custom_selection">
+        <location filename="../settings-ui/qml/SelectionWithCustomSetting.qml" line="53"/>
+        <source>Custom</source>
+        <translation>Własne</translation>
+    </message>
+</context>
+</TS>
